6731fb0ecf7f51a248604336e8417bff8b2df620
Anton Semjonov <anton@semjonov.de>
2018-08-08 00:53:48 +0200

    remove protobuf and makefile, replace with setuptools

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+setuptools.setup(
+  name = 'aenker',
+  version = '0.1.0',
+  author = 'Anton Semjonov',
+  author_email = 'anton@semjonov.de',
+  scripts = ['aenker']
+)
