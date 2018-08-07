d9aa8b13afe6afd896657cf7c5a045611029b65e
Anton Semjonov <anton@semjonov.de>
2018-08-07 22:35:50 +0200

    simplify aenker, remove aes-gcm and argon2, allow keyfile usage

--- aenker.py
+++ aenker.py
@@ -8,121 +8,105 @@ import argparse
 import getpass
 from base64 import b64encode, b64decode
 
-# protobuf serialization
-try: Aenker = AuthenticatedCiphertextBlob
-except NameError:
-  from aenker_pb2 import AuthenticatedCiphertextBlob as Aenker
-
 # authenticated encryption primitives
 # https://cryptography.io/en/latest/hazmat/primitives/aead/
-from cryptography.hazmat.primitives.ciphers import aead as AEAD
-
-# key derivation functions
-# https://argon2-cffi.readthedocs.io/en/stable/api.html
-from argon2.low_level import hash_secret_raw as argon2_raw, Type as argon2_type
-
-# prompt for input on stderr
-def einput(text): sys.stderr.write(text); return input()
-
-# getpass and encode
-passwd = lambda prompt='Enter Password: ': getpass.getpass(prompt).encode('utf-8')
-
-# split string at index
-split = lambda string, index: (string[:index], string[index:])
-
-# get aead cipher depending on arguments
-aead = lambda key, cipher: AEAD.AESGCM(key) if cipher == Aenker.cipher_t.Value('AESGCM') else AEAD.ChaCha20Poly1305(key)
-
-# key derivation wrappers
-class KDF:
-
-  argon2 = lambda password, blob :\
-    split(argon2_raw(password, blob.nonce, \
-    blob.kdf_opts.time_cost, 2**blob.kdf_opts.memory_cost, blob.kdf_opts.parallelism, \
-    hash_len=44, type=argon2_type.I), 12)
+from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305 as chacha
 
 # parse commandline arguments
 argparser = argparse.ArgumentParser()
 
 # arg_grp: input / output files
 grp_io = argparser.add_argument_group('file selection')
-grp_io.add_argument('file', nargs='?', help='input file (default: sys.stdin)')
+grp_io.add_argument('infile', nargs='?', help='input file (default: sys.stdin)')
 grp_io.add_argument('-o', '--out', metavar='file', help='output file (default: sys.stdout)')
 
 # arg: subcommand
 arg_cmd = argparser.add_mutually_exclusive_group()
 arg_cmd.add_argument('-e', '--encrypt', action='store_true', help='encrypt file (default)')
 arg_cmd.add_argument('-d', '--decrypt', action='store_true', help='decrypt file')
+arg_cmd.add_argument('-g', '--genkey', action='store_true', help='generate keyfile')
 
-# arg_grp: encryption options
-grp_options = argparser.add_argument_group('Encryption options', description="""
-The following options are only useful during encryption.
-Choices are saved in the serialized file.
-""")
-
-# arg_grp: key derivation mode
-grp_kdf = argparser.add_argument_group('Key generation')
+# arg_grp: key material mode
+grp_kdf = argparser.add_argument_group('key material')
 arg_kdf = grp_kdf.add_mutually_exclusive_group()
-arg_kdf.add_argument('-r', '--random', action='store_true', help='use a random key from os.urandom()')
-arg_kdf.add_argument('-p', '--password', action='store_true', help='use Argon2 to derive key (default)')
-grp_kdf.add_argument('--kdf-time-cost', type=int, help='KDF time cost / iterations (default: 21)', metavar='i')
-grp_kdf.add_argument('--kdf-memory-cost', type=int, help='KDF memory cost, power of two (default: 15)', metavar='m')
-grp_kdf.add_argument('--kdf-parallelism', type=int, help='KDF parallelism (default: 4)', metavar='p')
-
-
-# arg_grp: cipher algorithm
-grp_cipher = argparser.add_argument_group('Cipher selection')
-arg_cipher = grp_cipher.add_mutually_exclusive_group()
-arg_cipher.add_argument('-c', '--chacha20', action='store_true', help='use ChaCha20Poly1305 cipher (default)')
-arg_cipher.add_argument('-g', '--aes-gcm', action='store_true', help='use AES-GCM cipher')
+arg_kdf.add_argument('-r', '--random', action='store_true', help='random key from os.urandom() (default)')
+arg_kdf.add_argument('-k', '--key', help='32-byte base64-encoded key as argument')
+arg_kdf.add_argument('-f', '--file', help='32-byte base64-encoded key from file')
 
 args = argparser.parse_args()
 
+# read key from file
+def readkey(file):
+  with open(file, mode='rb') as k:
+    return k.read()
+
+# parse base64 string as 32 byte key
+def parsekey(st, length=32):
+  key = b64decode(st)
+  if len(key) != length:
+    raise ValueError(f'key has invalid length ({len(key)} != {length})')
+  return key
+
 # open files
 with \
-  open(args.file, mode='rb') if args.file else sys.stdin.buffer as infile  ,\
-  open(args.out,  mode='wb') if args.out else sys.stdout.buffer as outfile :
+  open(args.infile, mode='rb') if args.infile else sys.stdin.buffer as infile  ,\
+  open(args.out, mode='wb') if args.out else sys.stdout.buffer as outfile :
 
-  ae = Aenker()
+  # magic bytes and associated data
+  MAGIC = b'aenker'
+  MEK_AD = b'aenker media encryption key'
+  CTX_AD = b'aenker ciphertext'
+
+  # keygen
+  if args.genkey:
+
+    outfile.write(b64encode(os.urandom(32)) + b'\n')
 
   # decryption
-  if args.decrypt:
+  elif args.decrypt:
 
-    ae.ParseFromString(infile.read())
+    if MAGIC != infile.read(len(MAGIC)):
+      raise ValueError('invalid magic bytes')
 
-    # random key, stored nonce
-    if ae.kdf == ae.kdf_t.Value('None'):
-      nonce = ae.nonce
-      key = b64decode(einput('Enter Base64 encoded key: '))
-
-    # password-derived key and nonce, stored salt
+    if args.key is not None:
+      kek = args.key
+    elif args.file is not None:
+      kek = readkey(args.file)
     else:
-      nonce, key = KDF.argon2(passwd(), ae)
+      sys.stdin = open('/dev/tty')
+      kek = input('Enter Base64 encoded key: ')
 
-    # get cipher type and decrypt
-    message = aead(key, ae.cipher).decrypt(nonce, ae.text, None)
+    kek = parsekey(kek)
+
+    nonce = infile.read(12)
+    mek = infile.read(48)
+    ciphertext = infile.read()
+
+    mek = chacha(kek).decrypt(nonce, mek, MEK_AD)
+    message = chacha(mek).decrypt(nonce, ciphertext, CTX_AD)
+
     outfile.write(message)
 
   # encryption
   else:
 
-    # generate random key, store nonce
-    if args.random:
-      nonce = ae.nonce = os.urandom(12)
-      key = os.urandom(32)
-      print('Encryption key:', b64encode(key).decode(), file=sys.stderr)
+    # media encryption
+    mek = os.urandom(32)
+    nonce = os.urandom(12) # TODO: nonce is used both for mek and message encryption
 
-    # password-derived key and nonce, store salt
+    # generate random key encryption key
+    if args.key is not None:
+      kek = parsekey(args.key)
+    elif args.file is not None:
+      kek = parsekey(readkey(args.file))
     else:
-      ae.kdf = ae.kdf_t.Value('Argon2')
-      if args.kdf_time_cost:    ae.kdf_opts.time_cost   = args.kdf_time_cost
-      if args.kdf_memory_cost:  ae.kdf_opts.memory_cost = args.kdf_memory_cost
-      if args.kdf_parallelism:  ae.kdf_opts.parallelism = args.kdf_parallelism
-      ae.nonce = os.urandom(12)
-      nonce, key = KDF.argon2(passwd(), ae)
+      kek = os.urandom(32)
+      print('Encryption key:', b64encode(kek).decode(), file=sys.stderr)
 
-    # set cipher type and encrypt
-    if args.aes_gcm: ae.cipher = ae.cipher_t.Value('AESGCM')
-    ae.text = aead(key, ae.cipher).encrypt(nonce, infile.read(), None)
-    outfile.write(ae.SerializeToString())
+    ciphertext = chacha(mek).encrypt(nonce, infile.read(), CTX_AD)
+    mek = chacha(kek).encrypt(nonce, mek, MEK_AD)
 
+    outfile.write(MAGIC)
+    outfile.write(nonce)
+    outfile.write(mek)
+    outfile.write(ciphertext)
